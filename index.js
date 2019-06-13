6c839f87236bc9446c5a24623613115289b69a8f
killeraliens <killeraliens@outlook.com>
2019-06-12 18:10:48 -0700

    new list items can be added

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
 console.log('hey');
 
+function handleInput() {
+  const itemForm = $('#js-shopping-list-form');
+  itemForm.on('submit', function (e) {
+    e.preventDefault();
+    const inputField = $(this).find('#shopping-list-entry');
+    const userInput = inputField.val();
+    const listContainer = $('ul.shopping-list');
 
+    listContainer.prepend(`<li>
+      <span class="shopping-item">${userInput}</span>
+      <div class="shopping-item-controls">
+        <button class="shopping-item-toggle">
+          <span class="button-label">check</span>
+        </button>
+        <button class="shopping-item-delete">
+          <span class="button-label">delete</span>
+        </button>
+      </div>
+    </li>`);
+
+    inputField.val('');
+  });
+}
+
+
+handleInput();
 
